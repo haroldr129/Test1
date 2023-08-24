bc2988de6a0b0c14e2d5f1cea912907a0541a676
haroldr129 <haroldr129@gmail.com>
2023-08-23 21:26:02 -0500

    My first commit

---
+++ R/Suma.R
@@ -0,0 +1,10 @@
+#' Funcion para sumar dos numeros
+#' @param x un numero
+#' @param y un numero
+#' @return Resultado de la suma de los parametros x + y
+#' @examples
+#' suma (2+3)
+#' suma (4+5)
+suma <- function(x,y){
+  return(x+y)
+}
